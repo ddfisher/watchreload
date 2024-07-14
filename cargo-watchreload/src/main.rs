9cae6e5ca5511a1a67c184161cc74597e7e932f0
Danny Weinberg <FuegoFro@gmail.com>
2024-07-14 12:56:28 -0700

    Allow forwarding arguments to `wasm-pack`
    The motivation here is to pass the `--dev` flag to newer versions of `wasm-pack` since that will do nice things like skip size optimizations. This introduces a dependency on `clap` for more robust cli arg handling (including `--help` output), rather than the previous strategy of reading from the args directly.
    
    While here I noticed that if it failed to listen to the port, the `cargo watch` subcommand would continue to run, so I had it kill that process in that case.

--- cargo-watchreload/src/main.rs
+++ cargo-watchreload/src/main.rs
@@ -1,10 +1,29 @@
 use anyhow::Result;
+use clap::Parser;
 use env_logger::Env;
 use futures_util::SinkExt;
 use log::info;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::sync::watch::Receiver;
+
+#[derive(Debug, Parser)]
+/// Sets up a server for the web app to connect to via a websocket, then uses `cargo watch` to
+/// recompile the app when it changes and tells the app to reload itself via the websocket after
+/// the compilation is done.
+///
+/// Compiles the app via `wasm-pack`, which must already be installed and available on the `PATH`.
+struct CliArgs {
+    #[clap(long, default_value = "0.0.0.0")]
+    host: String,
+    #[clap(long, default_value = "9001")]
+    websocket_port: String,
+    #[clap(
+        trailing_var_arg = true,
+        help = "Forwarded to the `wasm-pack` invocation"
+    )]
+    wasm_pack_args: Vec<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -13,6 +32,19 @@ async fn main() -> Result<()> {
 
 async fn main_async() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // When invoked as `cargo watchreload` cargo will add `watchreload` as the first argument,
+    // which messes up Clap, so we strip it out.
+    let args = if std::env::args().nth(1) == Some("watchreload".into()) {
+        // Skip the 1th element
+        CliArgs::parse_from(
+            std::env::args()
+                .enumerate()
+                .filter(|(i, _)| *i != 1)
+                .map(|(_, a)| a),
+        )
+    } else {
+        CliArgs::parse()
+    };
 
     let (sender, receiver) = tokio::sync::watch::channel(());
     let mut signal_stream = signal(SignalKind::user_defined1())?;
@@ -25,33 +57,32 @@ async fn main_async() -> Result<()> {
         }
     });
 
-    let host = std::env::args()
-        .nth(2)
-        // .unwrap_or_else(|| format!("0.0.0.0:{websocket_port}"));
-        .unwrap_or_else(|| String::from("0.0.0.0"));
+    let address = format!("{}:{}", args.host, args.websocket_port);
 
-    let websocket_port = std::env::args()
-        .nth(3)
-        // .unwrap_or_else(|| format!("0.0.0.0:{websocket_port}"));
-        .unwrap_or_else(|| String::from("9001"));
-
-    let address = format!("{host}:{websocket_port}");
-
-    tokio::process::Command::new("cargo")
+    let mut child = tokio::process::Command::new("cargo")
         .args([
             "watch",
             "--shell",
-            "wasm-pack build --target web",
+            // TODO - Shell escape the args?
+            &format!(
+                "wasm-pack build --target web {}",
+                args.wasm_pack_args.join(" ")
+            ),
             "--shell",
             &format!("kill -USR1 {pid}"),
         ])
-        .env("WATCHRELOAD_PORT", websocket_port)
+        .env("WATCHRELOAD_PORT", args.websocket_port)
         .spawn()?;
     println!("Cargo watch is running");
 
     // Create the event loop and TCP listener we'll accept connections on.
-    let try_socket = TcpListener::bind(&address).await;
-    let listener = try_socket.expect("Failed to bind");
+    let listener = match TcpListener::bind(&address).await {
+        Ok(listener) => listener,
+        Err(e) => {
+            child.kill().await?;
+            panic!("Failed to bind: {e}");
+        }
+    };
     info!("Listening on: {}", address);
 
     while let Ok((stream, _)) = listener.accept().await {
